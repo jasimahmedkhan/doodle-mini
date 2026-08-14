2019d8139c83999be693479fe44a41bdaf0dc180
jasimahmedkhan <jasimahmedkhan@gmail.com>
2026-08-14 14:45:28 +0200

    initialize Spring Boot project

---
+++ src/main/java/com/minidoodle/schedular/DoodleMiniApplication.java
@@ -0,0 +1,13 @@
+package com.minidoodle.schedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodleMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoodleMiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minidoodle/schedular/DoodleMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minidoodle.schedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoodleMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
